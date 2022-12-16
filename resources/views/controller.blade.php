e497525316fc056014229c0cdf141aabc5d652e2
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-12-16 18:31:05 +0100

    add some style without scss

--- resources/views/controller.blade.php
+++ resources/views/controller.blade.php
@@ -10,13 +10,32 @@
 </head>
 
 <body>
-    <h1>Test controller</h1>
-    <ul>
-        @foreach ($movies as $movie)
-            {{-- oggetto o collezzione di oggetti ciò che arriva da model --}}
-            <li>{{ $movie->title }}</li>
-        @endforeach
-    </ul>
+    <h1 class="text-center">Movies</h1>
+    <div class="container">
+        <div class="row row-cols-4 g-4">
+
+            @foreach ($movies as $movie)
+                {{-- oggetto o collezzione di oggetti ciò che arriva da model --}}
+                <div class="col">
+
+                    <div class="card  text-bg-dark w-100">
+                        <img class="card-img-top" src="{{ $movie->image }}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text">{{ $movie->original_title }}</p>
+                        </div>
+                        <ul class="list-group list-group-flush text-bg-dark">
+                            <li class="list-group-item list-group-item-dark">Natinality: {{ $movie->nationality }}</li>
+                            <li class="list-group-item ">Data: {{ $movie->date }} </li>
+                            <li class="list-group-item list-group-item-dark">Vote: {{ $movie->vote }}</li>
+                        </ul>
+
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
 </body>
 
 </html>
