15f818afe8baec5485e5abae52902766888839d2
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-12-16 17:38:37 +0100

    get the table on laravel_model_controller with model and pass it to controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('controller');
+        $movies = Movie::all();
+        dd($movies);
+        return view('controller', compact('movies'));
     }
 }
